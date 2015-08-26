c9fc9144d9417d472b5161c7ab61827c5b1afa18
sarah11 <scmcgriff11@gmail.com>
2015-08-26 13:48:14 +0000

    Added try method

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825160125) do
+ActiveRecord::Schema.define(version: 20150825162113) do
 
   create_table "brands", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "categories", force: :cascade do |t|
