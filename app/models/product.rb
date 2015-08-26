f3f7b1c62ebcc3976e4331401022785067a68331
sarah11 <scmcgriff11@gmail.com>
2015-08-26 16:12:39 +0000

    Added storefront view with tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,6 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   
   has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/:style_missing.jpg"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
 
