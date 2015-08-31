7122e805112ead405b73b7117455aaa0722ff941
sarah11 <scmcgriff11@gmail.com>
2015-08-31 15:52:39 +0000

    Added Travis CI file

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,17 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :line_items, dependent: :destroy 
+  
+  validates :name, :address, :user_id, presence: true
+  
+  PAYMENT_TYPES = ["Check", "Credit Card", "Paypal"]
+  validates :payment_type, inclusion: PAYMENT_TYPES
+  
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item| 
+      item.cart_id = nil
+      item.order_id = id
+      line_items << item
+    end 
+  end
 end
