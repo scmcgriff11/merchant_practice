2f4fa64964ab4e93a0d03269ecaee587c6d7bf85
sarah11 <scmcgriff11@gmail.com>
2015-09-08 14:06:51 +0000

    Made an edit to the shopping cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,17 @@
 class Cart < ActiveRecord::Base
     has_many :line_items, dependent: :destroy
-    
+  
+  def add_product(product_id)
+   product = Product.find(product_id)
+   current_item = line_items.find_by(product_id: product_id)
+   if current_item
+     current_item.quantity += 1
+   else
+     current_item = line_items.build(product: product, price: product.price)
+   end
+   current_item
+end
+ 
     def subtotal
         line_items.to_a.sum {|item| item.total_price} 
     end
